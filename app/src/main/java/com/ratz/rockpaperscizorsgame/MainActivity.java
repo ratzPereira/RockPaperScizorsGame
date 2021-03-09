4bb4534e6b86394117f72e3ca223675a0d301b91
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-03-09 22:34:13 -0100

    Added logic and tested. is working. need add total score and refactor code

--- app/src/main/java/com/ratz/rockpaperscizorsgame/MainActivity.java
+++ app/src/main/java/com/ratz/rockpaperscizorsgame/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,18 +19,47 @@ public class MainActivity extends AppCompatActivity {
 
     public void playerChoose(String playerChoose) {
 
+        ImageView cpuHandImg = findViewById(R.id.cpuHand);
+
+        TextView result = findViewById(R.id.resultText);
+
+        int randomNumber = new Random().nextInt(3);
+        String[] hands = {"rock", "paper", "scizor"};
+
+        String cpuHand = hands[randomNumber];
+
+        switch (cpuHand) {
+            case "rock": cpuHandImg.setImageResource(R.drawable.pedra);
+                break;
+            case "paper": cpuHandImg.setImageResource(R.drawable.papel);
+                break;
+            case "scizor": cpuHandImg.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        if( cpuHand == "scizor" && playerChoose == "paper" || cpuHand == "rock" && playerChoose == "scizor" || cpuHand == "paper" && playerChoose == "rock") {
+            //cpu wins
+            result.setText("Computer wins!");
+        } else if (cpuHand == "paper" && playerChoose == "scizor" || cpuHand == "rock" && playerChoose == "scizor" || cpuHand == "paper" && playerChoose == "rock") {
+            // player wins
+            result.setText("You won!");
+        }else {
+            //draw
+            result.setText("Its a tie!");
+        }
+
     }
 
-    public void rockHand() {
-
+    public void rockHand(View view) {
+        this.playerChoose("rock");
     }
 
-    public void paperHand() {
-
+    public void paperHand(View view) {
+        this.playerChoose("paper");
     }
 
-    public void scizorHand() {
-
+    public void scizorHand(View view) {
+        this.playerChoose("scizor");
     }
 
     public void startGame(View view) {
