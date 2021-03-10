4492c20366e130dc261a70014bb727b183345379
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-03-10 10:44:13 -0100

    implemented score counter for player and computer. Demo finished

--- app/src/main/java/com/ratz/rockpaperscizorsgame/MainActivity.java
+++ app/src/main/java/com/ratz/rockpaperscizorsgame/MainActivity.java
@@ -11,6 +11,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    int cpuWins = 0;
+    int playerWins = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void playerChoose(String playerChoose) {
 
+
+
         ImageView cpuHandImg = findViewById(R.id.cpuHand);
 
         TextView result = findViewById(R.id.resultText);
+
+        TextView userScore = findViewById(R.id.playerScore);
+        TextView cpuScore = findViewById(R.id.cpuScore);
+
 
         int randomNumber = new Random().nextInt(3);
         String[] hands = {"rock", "paper", "scizor"};
@@ -37,18 +46,33 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        if( cpuHand == "scizor" && playerChoose == "paper" || cpuHand == "rock" && playerChoose == "scizor" || cpuHand == "paper" && playerChoose == "rock") {
+
+        if(     cpuHand == "scizor" && playerChoose == "paper" ||
+                cpuHand == "paper" && playerChoose == "rock" ||
+                cpuHand == "rock" && playerChoose == "scizor") {
             //cpu wins
+            cpuWins++;
+            System.out.println("CPU WON " + cpuWins);
             result.setText("Computer wins!");
-        } else if (cpuHand == "paper" && playerChoose == "scizor" || cpuHand == "rock" && playerChoose == "scizor" || cpuHand == "paper" && playerChoose == "rock") {
+            userScore.setText(String.valueOf(playerWins));
+
+        } else if (  playerChoose == "scizor" && cpuHand == "paper" ||
+                     playerChoose == "paper" && cpuHand == "rock" ||
+                     playerChoose == "rock" && cpuHand == "scizor") {
             // player wins
+            playerWins++;
             result.setText("You won!");
+            System.out.println("Player WON " + playerWins);
+
+            cpuScore.setText(String.valueOf(cpuWins));
+
         }else {
             //draw
             result.setText("Its a tie!");
         }
 
     }
+
 
     public void rockHand(View view) {
         this.playerChoose("rock");
@@ -60,24 +84,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void scizorHand(View view) {
         this.playerChoose("scizor");
-    }
-
-    public void startGame(View view) {
-
-      ImageView rockImg = findViewById(R.id.rock);
-      ImageView paperImg = findViewById(R.id.paper);
-      ImageView scizorImg = findViewById(R.id.scizor);
-
-
-      TextView userScore = findViewById(R.id.playerScore);
-      TextView cpuScore = findViewById(R.id.cpuScore);
-
-
-      TextView roundResult = findViewById(R.id.resultText);
-
-
-      int playerWins = 0;
-      int cpuWins = 0;
     }
 
 }
